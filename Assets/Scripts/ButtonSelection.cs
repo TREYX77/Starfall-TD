838cdf07e11e0607567781eafc56c45c9522b009
chomster <aardaefeakce@gmail.com>
2025-10-26 14:12:23 +0100

    Main menu designing

--- Assets/Scripts/ButtonSelection.cs
+++ Assets/Scripts/ButtonSelection.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ButtonSelection : MonoBehaviour
 {
     public void play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void Quit()
+    {
+       
+        Application.Quit();
+
+       
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #endif
     }
 }

69944af4a6d560bee8b27cc22b3c210e31a28465
no <119920297+TREYX77@users.noreply.github.com>
2025-10-26 21:48:02 +0100

    ewqe

--- Assets/Scripts/ButtonSelection.cs
+++ Assets/Scripts/ButtonSelection.cs
@@ -8,19 +8,38 @@ public class ButtonSelection : MonoBehaviour
 {
     public void play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Load VastForest (build index 1)
+        SceneManager.LoadScene(1);
     }
+
     public void levels()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        // Load LevelMenu (build index 2)
+        SceneManager.LoadScene(2);
+    }
+
+    public void level1()
+    {
+        // Load VastForest (build index 1)
+        SceneManager.LoadScene(1);
+    }
+
+    public void level2()
+    {
+        // Load another level (if added later)
+        SceneManager.LoadScene(3);
+    }
+
+    public void level3()
+    {
+        // Load another level (if added later)
+        SceneManager.LoadScene(4);
     }
 
     public void Quit()
     {
-       
         Application.Quit();
 
-       
         #if UNITY_EDITOR
         EditorApplication.isPlaying = false;
         #endif
